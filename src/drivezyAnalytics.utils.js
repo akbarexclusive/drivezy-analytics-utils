385429dc8ad2ddd0711d470f5d70fe81b9649c02
kamlesh nehra <kamlesh.nehra@drivezy.com>
2018-07-27 11:38:26 +0530

    small change

--- src/drivezyAnalytics.utils.js
+++ src/drivezyAnalytics.utils.js
@@ -54,10 +54,11 @@ export default class DrivezyAnalytics{
                 }else{
                     // exectues only if prod env
                     !function () {
-                        analytics = window.analytics = window.analytics || []; if (!analytics.initialize) if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice."); else {
-                            analytics.invoked = !0; analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "debug", "page", "once", "off", "on"]; analytics.factory = function (t) { return function () { var e = Array.prototype.slice.call(arguments); e.unshift(t); analytics.push(e); return analytics } }; for (var t = 0; t < analytics.methods.length; t++) { var e = analytics.methods[t]; analytics[e] = analytics.factory(e) } analytics.load = function (t) { var e = document.createElement("script"); e.type = "text/javascript"; e.async = !0; e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/analytics.js/v1/" + t + "/analytics.min.js"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(e, n) }; analytics.SNIPPET_VERSION = "4.0.0";
-                            analytics.load(ENV.SEGMENT_KEYS.WEB);
-                            analytics.page();
+                        webAnalytics = window.analytics = window.analytics || [];
+                        if (!webAnalytics.initialize) if (webAnalytics.invoked) window.console && console.error && console.error("Segment snippet included twice."); else {
+                            webAnalytics.invoked = !0; webAnalytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "debug", "page", "once", "off", "on"]; webAnalytics.factory = function (t) { return function () { var e = Array.prototype.slice.call(arguments); e.unshift(t); webAnalytics.push(e); return webAnalytics } }; for (var t = 0; t < webAnalytics.methods.length; t++) { var e = webAnalytics.methods[t]; webAnalytics[e] = webAnalytics.factory(e) } webAnalytics.load = function (t) { var e = document.createElement("script"); e.type = "text/javascript"; e.async = !0; e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/webAnalytics.js/v1/" + t + "/webAnalytics.min.js"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(e, n) }; webAnalytics.SNIPPET_VERSION = "4.0.0";
+                            webAnalytics.load(ENV.SEGMENT_KEYS.WEB);
+                            webAnalytics.page();
                         }
                     }();
                 }
